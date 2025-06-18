edb21944187f7ce649f31a7b6efba10f649468f6
ibrahimsrgn <101056719+ibrahimsrgn@users.noreply.github.com>
2025-06-18 22:54:12 +0300

    enemy animations

--- Assets/Scripts/Enemy/EnemyBase.cs
+++ Assets/Scripts/Enemy/EnemyBase.cs
@@ -6,11 +6,15 @@ public class EnemyBase : MonoBehaviour
     [SerializeField] private float movementSpeed;
     [SerializeField] private float hitRange;
     [SerializeField] private float maxHealth;
-     private float currentHealth;
+    [SerializeField] private Animator animator;
+    private float currentHealth;
     [SerializeField, Tooltip("Attack Per Second")] private float attackRate;
     private float nextAttackTime;
     public GameObject player;
-    
+    private SpriteRenderer spriteRenderer;
+    private bool isDead=false;
+    private bool playerInHitRange;
+
     public enum EnemyState
     {
         idle,
@@ -21,20 +25,27 @@ public class EnemyBase : MonoBehaviour
     [SerializeField] EnemyState state;
     public virtual void Start()
     {
-        currentHealth=maxHealth;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        currentHealth = maxHealth;
         state = EnemyState.chasing;
         movementSpeed = Random.Range(movementSpeed * 0.8f, movementSpeed * 1.2f);
 
     }
     public virtual void Update()
     {
-        if(Input.GetKeyDown(KeyCode.F))
+        if (!isDead)
         {
-            TakeDamage(Random.Range(1,10));
+        LookToPlayer();
+            playerInHitRange = Vector2.Distance(player.transform.position, transform.position) < hitRange;
+        }
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            TakeDamage(Random.Range(1, 10));
         }
         switch (state)
         {
             case EnemyState.idle:
+
                 break;
             case EnemyState.chasing:
                 ChasePlayer();
@@ -43,16 +54,29 @@ public class EnemyBase : MonoBehaviour
                 AttackToPlayer();
                 break;
             case EnemyState.die:
+                isDead = true;
                 Die();
                 break;
 
         }
     }
+    public virtual void Idle()
+    {
+        if(player==null)
+        {
+            animator.SetTrigger("isIdle");
+        }
+        else
+        {
+            ChangeEnemyState(EnemyState.chasing);
+        }
+    }
     public virtual void ChasePlayer()
     {
-        if (Vector2.Distance(player.transform.position, transform.position) > hitRange)
+        if (!playerInHitRange)
         {
             transform.position = Vector2.MoveTowards(transform.position, player.transform.position, movementSpeed * Time.deltaTime);
+            animator.SetTrigger("isChasing");
         }
         else
         {
@@ -61,36 +85,57 @@ public class EnemyBase : MonoBehaviour
     }
     public virtual void AttackToPlayer()
     {
-        if (Vector2.Distance(player.transform.position, transform.position) > hitRange)
+        if (!playerInHitRange)
         {
             ChangeEnemyState(EnemyState.chasing);
+
             return;
         }
 
         if (Time.time > nextAttackTime)
         {
             Debug.Log("Attack");
+            animator.SetTrigger("isAttacking");
             nextAttackTime = Time.time + 1f / attackRate;
             //Start attacl animation and fire projectile?
+        }
+        else
+        {
+            animator.SetTrigger("isIdle");
         }
     }
     public virtual void Die()
     {
         //Death animation?
         //Coin drop or exp reward?
+        //ölünce hala hasar alma animasyonu tetiklenebiliyor fixle**************************************************************
         Debug.Log(gameObject.name + " died");
-        Destroy(gameObject);
+        Destroy(gameObject,3);
     }
     public virtual void ChangeEnemyState(EnemyState state)
     {
+        if (this.state == state) return;
         this.state = state;
     }
     public void TakeDamage(float damage)
     {
         currentHealth -= damage;
-        if(currentHealth<= 0)
+        animator.SetTrigger("isTakingDamage");
+        if (currentHealth <= 0)
         {
+        animator.SetTrigger("isDead");
             ChangeEnemyState(EnemyState.die);
         }
+    }
+    public void HitToPlayer()
+    {
+        if (playerInHitRange)
+        {
+            Debug.Log("Player got hit");
+        }
+    }
+    private void LookToPlayer()
+    {
+        spriteRenderer.flipX = player.transform.position.x > transform.position.x;
     }
 }
