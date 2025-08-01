5afb7ea6737183fa65f186b39994b771f5772594
İbrahim SARGIN <ibrahimsarginnn@gmail.com>
2025-08-01 18:55:08 +0300

    Shop 0.2

---
+++ Assets/Scripts/Shop/ShopCard.cs
@@ -0,0 +1,14 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopCard : MonoBehaviour
+{
+    [SerializeField] private Image icon;
+    [SerializeField] private TextMeshProUGUI nameText;
+    [SerializeField] private TextMeshProUGUI costText;
+    [SerializeField] private TextMeshProUGUI descriptionText;
+    [SerializeField] private Button buyButton;
+
+    
+}
---
+++ Assets/Scripts/Shop/ShopItem.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+[CreateAssetMenu(fileName = "NewShopItem", menuName = "Scriptable Objects/ShopItem")]
+public class ShopItem : ScriptableObject
+{
+    public string itemName;
+    public Sprite icon;
+    public int cost;
+    public string description;
+
+    [Header("Weapon Unlock")]
+    public string weaponName;
+    public void UnlockWeapon()
+    {
+        if (!string.IsNullOrEmpty(weaponName))
+        {
+            OsmanAttack attack = FindFirstObjectByType<OsmanAttack>();
+            if (attack != null && !attack.weaponDictionary.ContainsKey(weaponName))
+            {
+                attack.weaponDictionary[weaponName].bought = true;
+                Debug.Log("Weapon bought: " + weaponName);
+            }
+        }
+    }
+}
